01dacf581fa6ffcdcf668255e96e16896ab8c1a6
Sári Cintia <sari.cintia1996@gmail.com>
2022-11-29 12:30:53 +0100

    pingvin kép beillesztése canvasba

--- java.js
+++ java.js
@@ -1,45 +1,13 @@
-var canvas = document.getElementById('viewport'),
-context = canvas.getContext('2d');
+
+
+var canvas = document.getElementById("viewport"),
+  context = canvas.getContext("2d");
 
 make_base();
 
-function make_base()
-{
-  base_image = new Image();
-  base_image.src = './picture/leftPinguin.jpg';
-  base_image.onload = function(){
-    context.drawImage(base_image, 0, 0);
-  }
-};
-
+function make_base() {
+  var base_image = document.querySelector(".pingvin");
+  context.drawImage(base_image, 0, 0,20,20);
+}
 
 make_base();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// pinguin figura
-/*var pinguin = function(){
-    this.ninckname = this.ninckname;
-    this.x = x;
-    this.y = y;
-    this.img = ;
-};
-
-
-
