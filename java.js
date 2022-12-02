02416260ef7e8800b4722b49f0d36464a476a61a
Sári Cintia <sari.cintia1996@gmail.com>
2022-12-02 10:58:13 +0100

    js elrendezés

--- java.js
+++ java.js
@@ -6,58 +6,31 @@ canvas.setAttribute("height", "1200"),
   context = canvas.getContext("2d");
 
 // 1. Alap adatok
+
 // 1.1 Pálya 
 let canvasWidth = 500 ; //jétkterület szélessége
 
-// 1.2 Pinvin 
+// 1.2 Pingvin 
 let pinguinSizeX=200;
 let pinguinSizeY=160;
 let pinguinY = 800;
 let pinguinX = ((canvasWidth+250 )/2); // pinvin kiindulási pontja
 let rightPressed = false ; // pingvin mozgatás ,jobb oldali gombnyomás
 let leftPressed = false; // pingvin mozgatás ,jobb oldali gombnyomás. Az alapértelmezett érték mindkettőnél azért van, falsemert az elején nem nyomják meg a vezérlőgombokat.
-let pingvinO = (canvas.width-pinguinSizeX)/2;
+
 
 // 1.3 halacska adatok
-let fishRadius=70;
+
 let fishSizeX =100; // hal méret az x tengelyen
 let fishSizeY = 70;// hal méret az y tengelyen
 let fishX = Math.random()*900; // képernyőn hol helyzekedjen el az x tengelyen méret max 900;
 let fishY =0; //// képernyőn hol helyzekedjen el az y tengelyen méret max 1190 min-60;
+let fx = 0; // hal mozgása az x tengelyen kiinduló pont
+let fy = -2; // hal mozgása az y tengelyen kiinduló pont
 
-let fx = 0; // hal mozgása az x tengelyen
-let fy = -2; // hal mozgása az y tengelyen
+// 2. elemek megrajzolása
 
-// pingvin adatok:
-
-// 2. Pingvin alap adatok
-
-/** 
-var pinguin = function (x,y){
-  this.y = y ;
- this.point = 0;
-};
-
-// Balra néző pingvin alap prototipusa 
-pinguin.prototype.drawLeft = function(){
-  let base_image = new Image();
-  base_image.src = "./picture/leftPinguin.png";
-   context.drawImage(base_image, pinguinX, this.y,200,160);
-};
-
-// jobbra néző pingvin prototipusa
-pinguin.prototype.drawRight = function(){
-  let base_image = new Image();
-  base_image.src = "./picture/rightPinguin.png";
-   context.drawImage(base_image, pinguinX, this.y,1000,1000);
-};
-
-// Pingvin megrajzolása
-var pingvin =new pinguin(400,1000);
-pingvin.drawLfet();
-pingvin.drawLRight();  */
-
-// pingvin megrajzolás
+// 2.1 pingvin megrajzolás
 
 function drawPinguinR (){
   let base_image = new Image();
@@ -66,26 +39,21 @@ function drawPinguinR (){
 
 };
 
-function drawPinguinL(){ //itt még hozzá kell adnom a HTMl-hez
+function drawPinguinL(){ //itt még hozzá kell adnom a HTMl-hez a másik oldalra dölő pingvint
   let base_image = new Image();
   base_image.src = "./picture/leftPinguin.png";
    context.drawImage(base_image, pinguinX, 1000,pinguinSizeX,pinguinSizeY);
 }
 
 
-// Hal rajzolás
+// 2.2  Hal rajzolás
 
-function drawFish(){ //itt még hozzá kell adnom a HTMl-hez
+function drawFish(){ 
   let fish_image = new Image();
   fish_image.src = "./picture/fish.png";
    context.drawImage(fish_image, fishX, fishY,fishSizeX,fishSizeY);
 };
 
-function drawFish2(){ //itt még hozzá kell adnom a HTMl-hez
-  let fish_image2 = new Image();
-  fish_image2.src = "./picture/2fish.png";
-   context.drawImage(fish_image2, 100, 1100,fishSizeX,fishSizeY);
-};
 
 
 
@@ -112,18 +80,17 @@ function keyUpHandler(event){
   };
 };
 
-// Pingvin mozgatás
+// 4. Pálya megrajzolása:
 
  function draw(){
   context.clearRect(0, 0, canvas.width, canvas.height); // ettől nem mosódik el a pingvin miközbe megy.
   drawPinguinL();
   drawFish();
 
-  fishX += fx;
-  fishY -= fy;
+  fishY -= fy; // hal mozgásának sebessége
  
 
-
+     // jobbra / balra gomb megnyomásakkor mennyivel menjen odébb a pingvin az x tengelyen
       if (rightPressed) {
         pinguinX = Math.min( pinguinX+7,canvasWidth+300);
       } else if (leftPressed){
@@ -131,61 +98,16 @@ function keyUpHandler(event){
       };
 
 
+// teszteléshez hogy megnézzem hogy válgozik az x és y tengely adatai:
+console.log("x :   "+Math.sqrt( Math.pow((pinguinX-fishY),2)));
+console.log("Y :   "+Math.sqrt(Math.pow( (pinguinY-fishY),2)));
 
-console.log(fishY + fy > canvas.height-fishRadius);
-console.log(fishX < pingvinO + pinguinSizeX);
- 
- 
- 
-     if(fishY + fy > canvas.height-fishRadius) {
-        if(fishX > pingvinO && fishX < pingvinO + pinguinSizeX) {
-            fy = -fy;
-        }
-        else {
-            alert("GAME OVER");
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
-        }
-    }
-      
+// a függvény amivel az alkapást tudom vizsgálni
+      if(Math.sqrt( Math.pow((pinguinX-fishX),2) +  Math.pow( (pinguinY-fishY),2) ) < 45){
+        console.log("elkapta")
+      }
  };
 
 
 
 const interval = setInterval(draw, 10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function make_base() {
-  var base_image = document.querySelector(".pingvin");
-  context.drawImage(base_image, 0, 0,200,160);}
-  
-  make_base();*/
-
-  /*function draw() {
-    let base_image = new Image();
-   base_image.src = "./picture/leftPinguin.png";
-    context.drawImage(base_image, 100,100);
-    console.log("OK");};*/
-  
